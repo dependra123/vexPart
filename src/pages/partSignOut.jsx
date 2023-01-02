d7dd8c3826aa711a7882abf1f77fc9f312819bee
dependra123 <dependra.khatri12@gmail.com>
2023-01-01 22:06:13 -0700

    gammer

--- src/pages/partSignOut.jsx
+++ src/pages/partSignOut.jsx
@@ -4,49 +4,57 @@ import { motion } from 'framer-motion';
 import './partSignOut.css';
 import { Link } from 'react-router-dom';
 
+
 // make a functions that will handle the input feilds
 
-const signOutInput = () => {
-    //check if the input feilds are empty and if they are, set a red border
-    let inputFeildCorrect = false;
-    let count = 0;
-    const handleChange = (e) => {
-        // check if user is done typing
-        if(e.target.value.length > 0){
-            count++;
-        }
-        if(count > 4){
-            inputFeildCorrect = true;
-        }
-    }
-
+const signOutInput = (index) =>{
+    const[verifed, setVerifed] = useState(false);
+    const [showVerfiy, setShowVerfiy] = useState(false);
     
     return (
         
-            <motion.form className="signOut" >
-                <input type="text" placeholder="Part Name" onChange={handleChange}/>
+            <motion.form className="signOut" key={index}>
+                <input type="text" placeholder="Part Name" />
                 <input type="text" placeholder="Part Size"/>
                 <input type="text" placeholder="Part Amount"/>
                 <input type="text" placeholder="Initials"/>
+                <motion.button className="material-symbols-outlined" style={{background:'green',scale:.75, borderRadius:90}} whileHover={{scale:1.1}} whileTap={{scale:0.85}} onClick={() => {
+                    setShowVerfiy(true);
+                }}>
+                    done
+                </motion.button>
+                {showVerfiy ? 
+                    <motion.div className="verify" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+                        {/* ask for password and username */}
+                        <input type="text" placeholder="Username"/>
+                    </motion.div>
+                 :null}
             </motion.form>
+            
     );
 };
 
 function PartSignOut() {
-   
+    const [feildsArray, setFeildsArray] = useState([signOutInput(0)])
     const {routeID} = useParams();
-    const [feildsArray, setFeildsArray] = useState([signOutInput()])
-    const [divPos, setDivPos] = useState(1);
+    
+    const [key, setKeys] = useState(1);
     const handleClick = () => {
       
-        setFeildsArray([...feildsArray, signOutInput()]);
-        setDivPos(divPos + 1);
+        setFeildsArray([...feildsArray, signOutInput(key)]);
+        setKeys(key + 1);
        
     };
     const handleRemove = (index) => {
+        let toRemove = feildsArray[index];
         const newFeildsArray = [...feildsArray];
-        newFeildsArray.splice(index, 1);
-        setFeildsArray(newFeildsArray);
+        newFeildsArray.forEach(feild => {
+            if(feild.key === toRemove.key){
+                console.log(newFeildsArray.indexOf(feild));
+                newFeildsArray.splice(newFeildsArray.indexOf(feild), 1);
+                setFeildsArray(newFeildsArray);
+            }
+        });
     };
     
 
@@ -66,10 +74,13 @@ function PartSignOut() {
             <motion.div className='feildContainder' >
                 {feildsArray.map((feild, index) =>
                   <motion.span key={index} animate={{y:-5}} transition={{type:'spring', damping:'7'}} initial={{y:-5,opacity:0.2}} whileInView={{y:0, opacity:1}} className="feildSpan">
-                    <motion.button onClick={handleRemove} style={{scale:0.6, borderRadius:60, background:'#B0272B'}} className="material-symbols-outlined" whileHover={{scale:.8, borderRadius:90, background:'#FF0000'}} whileTap={{scale:.5}}>
+                    <motion.button onClick={() =>{
+                        handleRemove(index);
+                        console.log("index: "+ index.toString());
+                    }} style={{scale:0.6, borderRadius:60, background:'#B0272B'}} className="material-symbols-outlined" whileHover={{scale:.8, borderRadius:90, background:'#FF0000'}} whileTap={{scale:.5}}>
                         close 
                     </motion.button>
-                    {feild} {index}
+                    {feild}
                 </motion.span>)}
             </motion.div>
             
